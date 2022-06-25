de829e7239505c41619a23747e06f2496ab02fc1
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-06-25 08:15:31 +0200

    feature - useCallback added to video 2

--- src/videos/video2/video2.tsx
+++ src/videos/video2/video2.tsx
@@ -1,18 +1,24 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 const Video2 = () => {
   const [number, setNumber] = useState(1);
   const [renderCounter, setRenderCounter] = useState(1);
 
-  const duplicateNumber = useMemo(() => {
-    console.log("duplicateNumber");
+  const duplicateNumberMemo = useMemo(() => {
+    console.log("duplicateNumber useMemo");
     return number * 2;
   }, [number]);
 
-  const duplicateNumberWithoutMemo = () => {
-    console.log("duplicateNumber without memo");
+  const duplicateNumberWithoutMemoCallback = () => {
+    console.log("duplicateNumber without memo & callback. Render counter: ", renderCounter);
     return number * 2;
   };
+
+  const duplicateNumberCallback = useCallback(() => {
+    // "renderCounter" only will be uptated when number changes
+    console.log("duplicateNumber useCallback. RenderCounter: ", renderCounter);
+    return number * 2;
+  }, [number]);
 
   const onChange = (event: any) => {
     setNumber(Number(event.target.value));
@@ -25,8 +31,9 @@ const Video2 = () => {
   return (
     <div>
       <input type="number" value={number} onChange={onChange} />
-      <div>Double: {duplicateNumber}</div>
-      <div>Double: {duplicateNumberWithoutMemo()}</div>
+      <div>Double (Memo): {duplicateNumberMemo}</div>
+      <div>Double (callback): {duplicateNumberCallback()}</div>
+      <div>Double: {duplicateNumberWithoutMemoCallback()}</div>
 
       <button onClick={reRender}>Re-render</button>
     </div>
