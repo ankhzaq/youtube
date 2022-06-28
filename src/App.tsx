22c222ba7a6b4a57aaaa34908d136e665ac2718c
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-06-28 17:14:32 +0200

    hotfix - reorder functions for the video

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Navigate, Routes, Route, } from "react-router-dom";
 import Video1 from './videos/video1';
 import Video2 from './videos/video2/video2';
+import { Button } from '@mui/material';
+
+function getVideoById(id: number) {
+  switch (id) {
+    case 0: return <Video1 />;
+    case 1: return <Video2 />;
+    default: return null;
+  }
+}
 
 function App() {
+  const [video, setVideo] = useState(null);
+  const videos = [
+    {id: 0, title: 'React question interview 1'},
+    {id: 1, title: 'useMemo & useCallback'},
+  ];
+
   return (
-    <BrowserRouter>
-        <Routes>
-          <Route path="/video1" element={<Video1 />} />
-          <Route path="/video2" element={<Video2 />} />
-          <Route path="*" element={<Navigate replace to="/video1" />} />
-        </Routes>
-    </BrowserRouter>
+    <>
+      { videos.map((video) => (
+        // @ts-ignore
+        <Button onClick={() => setVideo(getVideoById(video.id))}>{video.title}</Button>
+      ))}
+      {video}
+    </>
   );
 }
 
