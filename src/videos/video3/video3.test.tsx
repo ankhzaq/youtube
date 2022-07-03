ca9d2ae91c7fc9b10282d450b2e879807c5913eb
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-07-03 11:54:46 +0200

    feature - test video3 with waitFor

--- src/videos/video3/video3.test.tsx
+++ src/videos/video3/video3.test.tsx
@@ -1,23 +1,29 @@
 import React from 'react'
-import {render, fireEvent, screen} from '@testing-library/react'
+import { render, fireEvent, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/extend-expect'
 import Video3 from './video3'
 
-test('testing input value', async () => {
+test('testing input value',
+  async () => {
 
-  render(<Video3 />)
+    render(<Video3/>)
 
-  let inputValue = screen.getByTestId('inputValue');
+    let inputValue = screen.getByTestId('inputValue');
 
-  // @ts-ignore
-  expect(inputValue.value).toEqual("0")
+    // @ts-ignore
+    expect(inputValue.value).toEqual('0')
 
-  const plusBtn = screen.getByTestId('plusBtn');
+    const plusBtn = screen.getByTestId('plusBtn');
 
-  fireEvent.click(plusBtn);
+    fireEvent.click(plusBtn);
 
-  inputValue = screen.getByTestId('inputValue');
+    inputValue = screen.getByTestId('inputValue');
 
-  // @ts-ignore
-  expect(inputValue.value).toEqual("1")
-})
+    // @ts-ignore
+    expect(inputValue.value).toEqual('1');
+
+    // @ts-ignore
+    const changedMessage = await screen.getByTestId('changedMessage')
+    expect(changedMessage).toBeInTheDocument();
+  })
--- src/videos/video3/video3.tsx
+++ src/videos/video3/video3.tsx
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Video3 = () => {
   const [value, setValue] = useState(0);
+  const [showChangedMessage, setShowChangedMessage] = useState(false);
+
+  useEffect(() => {
+    setShowChangedMessage(true);
+  }, [value]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowChangedMessage(false);
+    }, 1000)
+  }, [showChangedMessage])
 
   return (
-    <>
+    <div>
       <input data-testid="inputValue" value={value} />
       <button data-testid="plusBtn" onClick={() => setValue(value + 1)}>plus</button>
-    </>
+      {showChangedMessage && (
+        <div data-testid="changedMessage">Value has changed</div>
+      )}
+    </div>
   );
 }
 export default Video3;
